c4170429a92ba5b6f9ffbbe48b42b9d8de5cabc3
openwater301 <openwater301@hanmail.net>
2023-07-13 10:16:22 +0900

    10차수정완료

---
+++ src/components/TodoHeader.vue
@@ -0,0 +1,19 @@
+<template>
+  <header>
+    <h1>todos</h1>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+  h1 {
+    color: rgb(151, 71, 71);
+    font-weight: 900;
+    margin: 2.5rem 0 1.5rem;
+  }
+</style>
